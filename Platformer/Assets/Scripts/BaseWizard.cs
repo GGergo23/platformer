fd9cb9ddefbe1e94d39befe81063affce3a3b8e7
Diák_31 <Dik_31@LAPTOP-JMTLDC4M>
2019-10-26 11:58:08 +0200

    BaseWizard

---
+++ Platformer/Assets/Scripts/BaseWizard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class BaseWizard : BaseClassScript
+{
+    public BaseWizard (){
+        ClassName="Wizard";
+        Strength=2;
+        Health=20;
+        Intelligent=10;
+        Damage=3*Intelligent;
+        Shoot=true;
+    }
+}
