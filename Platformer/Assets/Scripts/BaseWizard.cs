7cd9bef051afcb91240e14f86c9126f9f660817e
Diak54 <Diak54@LAPTOP-KHBJ5VHJ>
2019-11-09 20:00:39 +0100

    branch fixed

--- Platformer/Assets/Scripts/BaseWizard.cs
+++ Platformer/Assets/Scripts/BaseWizard.cs
@@ -5,8 +5,8 @@ public class BaseWizard : BaseClassScript
     public BaseWizard() {
         ClassName = "Wizard";
         Strength = 5;
-        Health = 5;
-        Intelligent = 5;
+        Health = 4;
+        Intelligent = 7;
         Damage = Strength * 2;
         Shoot = true;
     }
