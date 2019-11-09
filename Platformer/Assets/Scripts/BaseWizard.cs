e155016d76178a30a166d690c76352dc8c5c959c
Diak54 <Diak54@LAPTOP-KHBJ5VHJ>
2019-11-09 19:54:42 +0100

    wizard script fixed, assets added

--- Platformer/Assets/Scripts/BaseWizard.cs
+++ Platformer/Assets/Scripts/BaseWizard.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BaseWizard : BaseClassScript
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public BaseWizard() {
+        ClassName = "Wizard";
+        Strength = 5;
+        Health = 5;
+        Intelligent = 5;
+        Damage = Strength * 2;
+        Shoot = true;
     }
 }
